06fc0a1dfac0233e9a1bbd571a92b5b6fe57ae81
Esteban Rojas Londoño <esteban7590@hotmail.com>
2023-08-02 16:15:54 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
 import content from "../content.json";
 
 export default function Home() {
-  return <h1>{content.heading}</h1>;
+  return (
+    <>
+      <h1>{content.heading}</h1>
+      <Image
+        // src to image on oublic folder
+        src={content.img_1}
+        width={500}
+        height={500}
+        alt="image"
+      />
+    </>
+  );
 }
